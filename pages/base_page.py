453e02572c910c4fd1a7af41a772891f02807055
Furkan Kızıltepe <furkan.kiziltepe@gmail.com>
2026-04-03 22:15:16 +0300

    Add WebDriverWait and expected_conditions to BasePage for improved element handling

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,6 +1,10 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 class BasePage:
     def __init__(self, driver):
         self.driver = driver
+        self.wait = WebDriverWait(driver, 10)
+        self.EC = EC
 
     def open(self, url):
         self.driver.get(url)
