9f572d2e74f8066e486a892e7e56c502511a66db
Furkan Kızıltepe <furkan.kiziltepe@gmail.com>
2026-04-03 22:15:37 +0300

    Update QA positions locator and add wait condition in CareersPage class

--- pages/careers_page.py
+++ pages/careers_page.py
@@ -1,13 +1,15 @@
 from selenium.webdriver.common.by import By
 from pages.base_page import BasePage
 
+
 class CareersPage(BasePage):
 
     SEE_ALL_TEAMS = (By.XPATH, "//a[contains(text(),'See all teams')]")
-    QA_TEAM = (By.XPATH, "//h3[contains(text(),'Quality Assurance')]")
+    QA_OPEN_POSITIONS = (By.XPATH, "//*[@id='open-roles']//*[contains(@data-department,'Quality Assurance')]//a[contains(text(),'5 Open Positions')]")
 
     def click_see_all_teams(self):
         self.driver.find_element(*self.SEE_ALL_TEAMS).click()
 
     def open_qa_positions(self):
-        self.driver.find_element(*self.QA_TEAM).click()
+        self.wait.until(self.EC.visibility_of_element_located(self.QA_OPEN_POSITIONS))
+        self.driver.find_element(*self.QA_OPEN_POSITIONS).click()
