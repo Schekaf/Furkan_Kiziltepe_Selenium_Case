3fc702d442a460e2eda967f3a533c277b8cd9e74
Furkan Kızıltepe <furkan.kiziltepe@gmail.com>
2026-04-04 12:33:35 +0300

    Update insider test to include assertion message and adjust QA job validation logic

--- tests/test_insider.py
+++ tests/test_insider.py
@@ -16,7 +16,7 @@ def test_insider(browser):
         home = HomePage(driver)
         home.open("https://insiderone.com/")
         assert home.is_loaded()
-        assert home.has_main_blocks()
+        assert home.has_main_blocks(), "Insider home page does not have main blocks!"
 
         # 2. Careers page
         careers = CareersPage(driver)
@@ -26,9 +26,11 @@ def test_insider(browser):
 
         # 3. QA jobs validation
         qa = QAPage(driver)
-        #qa.validate_jobs()
-        #qa.validate_departments()
-        #qa.validate_locations()
+        qa.validate_jobs()
+        #Note: Couldn't understand the requirement of this validation. If we check the current open positions, there are some positions that do not contain "Quality Assurance" in their title but they are still related to QA department. So I commented this validation for now. Please let me know if you want me to change it.
+        #qa.validate_all_jobs_contain_qa()
+        qa.validate_departments()
+        qa.validate_locations()
 
         # 4. Apply button check
         qa.click_apply()
